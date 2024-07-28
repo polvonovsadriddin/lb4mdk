ecf0eb95b09ff2f1bdc36a132e9fe680a30074ff
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2024-07-29 01:40:56 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab4mdk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Notes copiedNote = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        // Добавление обработчиков событий для заголовков столбцов
+
+    }
+    ObservableCollection<Notes> note = new ObservableCollection<Notes>();
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+
+        {
+
+
+            note.Add(new Notes(tbName.Text +" "+tbSurname.Text, int.Parse(tbNumber.Text), DateOnly.FromDateTime(new DateTime(int.Parse(tbyear.Text),
+                                                             int.Parse(tbmonth.Text),
+                                                             int.Parse(tbday.Text)))));
+           
+            tbName.Text = "";
+            tbSurname.Text = "";
+            tbday.Text = "";
+            tbyear.Text = "";
+            tbmonth.Text = "";
+            tbNumber.Text = "";
+            mylistview.ItemsSource = note;
+            CountEl.Text = $"Добавлено {note.Count} елементов";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           List<Notes> sortedlist =note.ToList();
+            sortedlist.Sort();
+            mylistview.ItemsSource = sortedlist;
+        }
+        private void mylistview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (mylistview.SelectedItem != null)
+            {
+                copiedNote = (Notes)((Notes)mylistview.SelectedItem).Clone();
+            }
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (copiedNote != null)
+            {
+                note.Add(copiedNote);
+
+            }
+            copiedNote = null;
+        }
+
+       
+    }
+}
+
+
+    
+  
+
+
+    
+    
+
+    
---
+++ Notes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4mdk
+{
+    public class Notes :ICloneable,IComparable<Notes>
+    {
+        public string FullName { get; set; }
+        public long Number { get; set; }
+        public DateOnly  BirthDate { get; set; }
+        public Notes(string fullName, int number, DateOnly birthDate) 
+        {
+            FullName= fullName;
+            Number= number;
+            BirthDate= birthDate;
+        }
+        
+        public object Clone()
+        {
+            return new Notes(this.FullName, (int)this.Number, this.BirthDate);
+        }
+       
+
+        public int CompareTo(Notes obj)
+        {
+            int nameComparison = string.Compare(this.FullName, obj.FullName, StringComparison.OrdinalIgnoreCase);
+            if (nameComparison != 0)
+            {
+                return nameComparison; 
+            }
+            return this.BirthDate.CompareTo(obj.BirthDate);
+
+        }
+    }
+}
